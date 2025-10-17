0cd71a134ee33f0c453512db63925b0e43e61575
Whit Waldo <whit.waldo@innovian.net>
2025-10-17 08:49:33 -0500

    Initial commit
    Signed-off-by: Whit Waldo <whit.waldo@innovian.net>

---
+++ Solution1.AppHost/AppHost.cs
@@ -0,0 +1,40 @@
+using CommunityToolkit.Aspire.Hosting.Dapr;
+
+var builder = DistributedApplication.CreateBuilder(args);
+
+var cache = builder.AddRedis("cache").WithDbGate();
+var daprState = builder.AddDaprStateStore("statestore");
+
+const int rabbitPort = 5672;
+var username = builder.AddParameter("uesrname", "guest", true);
+var password = builder.AddParameter("password", "guest", true);
+var rabbitmq = builder.AddRabbitMQ("rabbitmq", username, password, rabbitPort).WithManagementPlugin();
+
+var daprPubSub = builder.AddDaprPubSub("pubsub-rabbit", new DaprComponentOptions
+	{
+		LocalPath = Path.Combine("../.dapr", "components", "pubsub-rabbit.yaml"),
+	})
+	.WithMetadata("connectionString", $"amqp://{username}:{password}@localhost:{rabbitPort}")
+	.WaitFor(rabbitmq);
+
+builder.AddProject<Projects.Web>("web")
+	.WithHttpHealthCheck("/health")
+	.WithReference(cache)
+	.WithReference(rabbitmq)
+	.WithDaprSidecar(b =>
+	{
+		b.WithOptions(new DaprSidecarOptions
+			{
+				AppHealthCheckPath = "/health",
+				EnableAppHealthCheck = true,
+				AppId = "web",
+				AppPort = 5124,
+				DaprGrpcPort = 50001,
+				DaprHttpPort = 3500,
+				MetricsPort = 9090
+			})
+			.WithReference(daprPubSub)
+			.WithReference(daprState);
+	});
+
+builder.Build().Run();
